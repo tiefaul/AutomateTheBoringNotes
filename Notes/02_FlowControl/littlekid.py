3f35b347c50e3ce109c1c0443d1d8a871dc408f2
TieFaul <tylerfaulhaber@gmail.com>
2023-12-07 19:35:30 -0600

    Push 02

---
+++ Notes/02_FlowControl/littlekid.py
@@ -0,0 +1,11 @@
+name = 'Carol'
+age = 3000
+if name == 'Alice':
+    print('Hi, Alice')
+elif age < 12:
+    print('You are not Alice, kiddo')
+else:
+    print('You are neither Alice nor a little kid.')
+
+# There is always exactly one if statement. Any elif statements you need should follow the if statement. 
+# Second, if you want to be sure that at least one clause is executed, close the structure with an else statement
