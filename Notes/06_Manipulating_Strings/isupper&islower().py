722f0a0d4ee8787bb85fff6dc87f9224d0327d39
TieFaul <tylerfaulhaber@gmail.com>
2023-12-12 11:51:42 -0600

    20231212

---
+++ Notes/06_Manipulating_Strings/isupper&islower().py
@@ -0,0 +1,11 @@
+spam = 'Hello, world'
+
+print(spam.islower()) # returns false because everything is not lowercase
+
+print(spam.isupper()) # returns false because everythign is not uppercase
+
+print('HELLO'.isupper()) # prints true becasuse hello is capitalized
+
+print('Hello'.upper()) # prints hello out in all caps
+
+print('HELLO'.lower().islower()) # prints out true because of the .lower() 
