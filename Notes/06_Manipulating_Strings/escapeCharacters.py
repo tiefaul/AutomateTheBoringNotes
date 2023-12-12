90b9ccfcaf667ac979fcf41116870399a83d2972
TieFaul <tylerfaulhaber@gmail.com>
2023-12-11 19:56:23 -0600

    Test

--- Notes/06_Manipulating_Strings/escapeCharacters.py
+++ Notes/06_Manipulating_Strings/escapeCharacters.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 spam = 'Say hi to Bob\'s mother.' # \' and \" lets you put single quotes and double quotes inside your strings, respectively. \ is the break character
 
 print(spam)
@@ -16,8 +15,3 @@ print("""Dear Alice,
 Eve's cat has been arrested for catnapping and cat burglary.
 Sincerely, 
 Bob""") # Triple " or ' can be used to print out something like this
-=======
-spam = 'Say hi to Bob\'s mother.' # \' \" lets you put single quotes and double quotes inside your strings, respectively. \ is the break character
-
-print(spam)
->>>>>>> f6073c42e0bef91a2b5095471db91e8d8a8b458d
