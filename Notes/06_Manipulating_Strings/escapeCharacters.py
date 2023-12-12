9498508af154f50241568c7d42e089ad6365a117
TieFaul <tylerfaulhaber@gmail.com>
2023-12-11 19:51:03 -0600

    Test

--- Notes/06_Manipulating_Strings/escapeCharacters.py
+++ Notes/06_Manipulating_Strings/escapeCharacters.py
@@ -1,3 +1,17 @@
-spam = 'Say hi to Bob\'s mother.' # \' \" lets you put single quotes and double quotes inside your strings, respectively. \ is the break character
+spam = 'Say hi to Bob\'s mother.' # \' and \" lets you put single quotes and double quotes inside your strings, respectively. \ is the break character
 
 print(spam)
+print('')
+
+print('Hello there! \nHow are you? \nI\'m doing fine.') # The "\n" lets you print a new line
+
+print('')
+
+print(r'That is Carol\'s cat.') # The r string completely ignores all escape characters and prints any backlash that appears in the string (r string is also known as a raw string)
+
+print('')
+
+print("""Dear Alice,
+Eve's cat has been arrested for catnapping and cat burglary.
+Sincerely, 
+Bob""") # Triple " or ' can be used to print out something like this
