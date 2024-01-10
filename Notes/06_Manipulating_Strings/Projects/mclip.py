c468c90509e6fac4c6e1400dcd6e69ecc53626c6
TieFaul <tylerfaulhaber@gmail.com>
2024-01-09 20:35:24 -0600

    20240109

---
+++ Notes/06_Manipulating_Strings/Projects/mclip.py
@@ -0,0 +1,7 @@
+#! Python3
+
+#mclip.py - A multi-clipboard program.
+
+TEXT = {'agree': """Yes, I agree. That sounds fine to me.""",
+        'busy': """Sorry, can we do this later this week or next week?""",
+        'upsell': """Would you consider making this a monthly donation?"""}
