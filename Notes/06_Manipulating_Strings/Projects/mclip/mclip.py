a0ba9020e7a5a8f69865fd2b3fe883c36b8e073a
TieFaul <tylerfaulhaber@gmail.com>
2024-01-12 19:05:24 -0600

    20240112

--- Notes/06_Manipulating_Strings/Projects/mclip/mclip.py
+++ Notes/06_Manipulating_Strings/Projects/mclip/mclip.py
@@ -10,10 +10,10 @@ if len(sys.argv) < 2:
     print('Usage: python mclip.py [keyphrase] - copy phrase text')
     sys.exit()
 
-stop = sys.argv[1]  # first command line arg is the keyphrase
+keyphrase = sys.argv[1]  # first command line arg is the keyphrase
 
-if stop in TEXT:
-    pyperclip.copy(TEXT[stop])
-    print('Text for ' + stop + ' copied to clipboard.')
+if keyphrase in TEXT:
+    pyperclip.copy(TEXT[keyphrase])
+    print('Text for ' + keyphrase + ' copied to clipboard.')
 else:
-    print('There is no text for ' + stop)
+    print('There is no text for ' + keyphrase)
