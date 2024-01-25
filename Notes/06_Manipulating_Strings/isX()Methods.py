2f4562bc09f9430c8f3c943a06859fa8bd620941
TieFaul <tylerfaulhaber@gmail.com>
2024-01-24 20:17:41 -0600

    20240124

--- Notes/06_Manipulating_Strings/isX()Methods.py
+++ Notes/06_Manipulating_Strings/isX()Methods.py
@@ -1,7 +1,8 @@
-spam = "Random"
+spam = "123"
 
-spam.isalpha() # Returns True if the string consists only of letters and isn't blank
-spam.isalnum() # Returns True if the string consists only of letters and numbers and is not blank
-spam.isdecimal() # Returns True if the string consists only of numberic characters
-spam.isspace() # Returns True if the string consists only of spaces, tabs, and newlines
-spam.istitle() # Returns True if the string consists only of words that begin with an upper-case letter followed by only lowercase letters
+# spam.isalpha() # Returns True if the string consists only of letters and isn't blank
+# spam.isalnum() # Returns True if the string consists only of letters and numbers and is not blank
+# spam.isdecimal() # Returns True if the string consists only of numberic characters
+# spam.isspace() # Returns True if the string consists only of spaces, tabs, and newlines
+# spam.istitle() # Returns True if the string consists only of words that begin with an upper-case letter followed by only lowercase letters
+print(bool(spam.isnumeric()))
--- Notes/test.py
+++ Notes/test.py
@@ -1,1 +1,15 @@
 # Testing page
+print("List things you have to do today, leave blank to close out:")
+
+list = {}
+
+def toDo_list():
+    while True:
+        add_list = input()
+        if add_list.isalpha():
+            list[add_list] = " "
+        elif add_list == "":
+            break
+toDo_list()
+print(list)
+#complete_list()
