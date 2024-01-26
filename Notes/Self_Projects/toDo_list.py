4a7558abdf101faf161bebe0b3811635fec3b7a4
TieFaul <tylerfaulhaber@gmail.com>
2024-01-25 19:59:48 -0600

    20240125

---
+++ Notes/Self_Projects/toDo_list.py
@@ -0,0 +1,34 @@
+#! Python3
+# First self project
+
+def toDo():
+    toDo_list = {}
+    while True:
+        list = input("List something you need to do and press 'Enter'. To exit type 'exit': ")
+
+        if list.lower()  == 'exit':
+            break
+        
+        toDo_list[list] = " "
+
+    for task in toDo_list.keys():
+        print(f"{task}:")
+        
+
+    while True:
+        completed = input("Type things you have completed to add an 'X' next to it: ")
+
+        if completed in toDo_list:
+            toDo_list[completed] = 'X'
+
+        if completed == 'exit':
+            break
+        
+        elif completed not in toDo_list:
+            print("This is not in your list.")
+        
+        
+    for task, status in toDo_list.items():
+        print(f"{task} : {status}")
+
+toDo()
