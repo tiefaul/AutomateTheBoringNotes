82824c8f28a9928fa34ede3b27a6bd659183a349
TieFaul <tylerfaulhaber@gmail.com>
2024-03-09 13:03:08 -0600

    20240309

--- Notes/07_PatternMatching/Projects/strong_password_detection.py
+++ Notes/07_PatternMatching/Projects/strong_password_detection.py
@@ -1,18 +1,20 @@
 import re
-def Pass_detection(pw):
+
+def pw_detection(pw):
     if len(pw) < 8:
-       return False
-    elif re.search('[0-9]|[A-Z]|[a-z]', pw) is None:
+        return False
+    elif re.search('[0-9]', pw) is None:
+        return False
+    elif re.search('[A-Z]', pw) is None:
+        return False
+    elif re.search('[a-z]', pw) is None:
         return False
     else:
         return True
-
-pw = input('Enter your password : ')
-if Pass_detection(pw):
-    print('Good, your password is strong!')
-else:
-    print('''
-    The Password should at least be eight characters long,
-    contains both uppercase and lowercase characters,
-    and has at least one digit.
-    ''') 
+    
+if __name__ == '__main__':
+    pw = input('Enter your password: ')
+    if pw_detection(pw):
+        print('Good, your password is strong!')
+    else:
+        print('The password is at least eight characters long, contains both uppercase and lowercase characters, and at least one digit.')
